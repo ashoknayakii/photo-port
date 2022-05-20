7d2c7b96906938eea6d62f373b772c9915e309db
Ashok Nayak <ashoknayak@MacBook-Air.local>
2022-05-19 23:57:13 -0400

    testing for contact component finished still debugging needed for Nav tests

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -3,22 +3,27 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
 
-// const categories = [
-//     { name: 'portraits', description: 'Portraits of people in my life' }
-//   ]
-//   const mockCurrentCategory = jest.fn();
-//   const mockSetCurrentCategory = jest.fn();
+const categories = [
+    { name: 'portraits', description: 'Portraits of people in my life' }
+  ]
 
 afterEach(cleanup);
 
 describe('Nav component', () => {
-    // baseline test
 
+    const mockCurrentCategory = jest.fn();
+    const mockSetCurrentCategory = jest.fn();
+    const mockContactSelected = jest.fn();
+    const mockSetContactSelected = jest.fn();
+    // baseline test
+  
     it('renders', () => {
         render(<Nav 
-        // categories={categories}
-        // mockSetCurrentCategory={mockSetCurrentCategory}
-        // currentCategory={mockCurrentCategory}
+        categories={categories}
+        mockSetCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+        contactSelected={mockContactSelected}
+        setContactSelected={mockSetContactSelected}
         />);
     });
 
